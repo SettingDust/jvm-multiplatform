a109c8e57bd2dbcf9bc9fe01d5d6d73172cdd0a2
Ashley Wright <ashley@msrandom.net>
2025-12-07 19:19:04 -0500

    Support kotlin 2.2.20 for multiplatform

--- virtual-source-sets/gradle-plugin/src/main/kotlin/net/msrandom/virtualsourcesets/JavaVirtualSourceSetsPlugin.kt
+++ virtual-source-sets/gradle-plugin/src/main/kotlin/net/msrandom/virtualsourcesets/JavaVirtualSourceSetsPlugin.kt
@@ -5,9 +5,8 @@ package net.msrandom.virtualsourcesets
 import net.msrandom.virtualsourcesets.model.VirtualSourceSetModelBuilder
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.file.ConfigurableFileCollection
+import org.gradle.api.model.ObjectFactory
 import org.gradle.api.plugins.JavaPlugin
-import org.gradle.api.provider.HasMultipleValues
 import org.gradle.api.provider.Provider
 import org.gradle.api.provider.ProviderFactory
 import org.gradle.api.tasks.SourceSet
@@ -22,19 +21,9 @@ import org.gradle.tooling.provider.model.ToolingModelBuilderRegistry
 import org.jetbrains.kotlin.gradle.InternalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.KotlinSingleJavaTargetExtension
 import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
-import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSetContainer
-import org.jetbrains.kotlin.gradle.tasks.K2MultiplatformStructure
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import javax.inject.Inject
-import kotlin.reflect.KProperty1
-import kotlin.reflect.full.memberProperties
-import kotlin.reflect.jvm.isAccessible
-
-@Suppress("UNCHECKED_CAST")
-private val commonSourceSet = KotlinCompile::class.memberProperties
-    .first { it.name == "commonSourceSet" }
-    .apply { isAccessible = true } as KProperty1<KotlinCompile, ConfigurableFileCollection>
 
 private const val KOTLIN_JVM = "org.jetbrains.kotlin.jvm"
 
@@ -43,8 +32,8 @@ private val KotlinCompile.isK2: Provider<Boolean>
         .orElse(KotlinVersion.DEFAULT)
         .map { it >= KotlinVersion.KOTLIN_2_0 }
 
-fun <T : Any> KotlinCompile.addK2Argument(property: HasMultipleValues<T>, value: () -> T) {
-    property.addAll(isK2.map {
+fun KotlinCompile.addK2Argument(value: () -> String) {
+    compilerOptions.freeCompilerArgs.addAll(isK2.map {
         if (it) {
             listOf(value())
         } else {
@@ -78,52 +67,67 @@ open class JavaVirtualSourceSetsPlugin @Inject constructor(private val modelBuil
 
     private fun SourceSet.addKotlinCommonSources(
         kotlin: KotlinSourceSetContainer,
+        objectFactory: ObjectFactory,
         providerFactory: ProviderFactory,
         dependency: SourceSet,
         info: SourceSetStaticLinkageInfo,
         compileTask: KotlinCompile,
+        handledSourceSetNames: MutableSet<String> = hashSetOf(),
     ) {
         val kotlinSourceSet = kotlin.sourceSets.getByName(name)
         val kotlinDependency = kotlin.sourceSets.getByName(dependency.name)
 
-        fun addFragment(sourceSet: KotlinSourceSet) {
-            if (compileTask.multiplatformStructure.fragments.get().any { it.fragmentName == sourceSet.name }) {
-                return
-            }
-
-            compileTask.addK2Argument(compileTask.multiplatformStructure.fragments) {
-                K2MultiplatformStructure.Fragment(sourceSet.name, sourceSet.kotlin.asFileTree)
-            }
+        val newFragments = listOf(kotlinSourceSet, kotlinDependency).filter {
+            handledSourceSetNames.add(it.name)
         }
-
-        compileTask.addK2Argument(compileTask.multiplatformStructure.refinesEdges) {
-            K2MultiplatformStructure.RefinesEdge(kotlinSourceSet.name, kotlinDependency.name)
-        }
-
-        addFragment(kotlinSourceSet)
-        addFragment(kotlinDependency)
-
-        val emptyList: Provider<List<K2MultiplatformStructure.RefinesEdge>> = providerFactory.provider { emptyList() }
 
         val weakLinks = info.weakTreeLinks(dependency)
+        val emptyList: Provider<List<String>> = providerFactory.provider { emptyList() }
 
-        compileTask.multiplatformStructure.refinesEdges.addAll(compileTask.isK2.flatMap {
-            if (it) {
-                weakLinks.map {
-                    it.map { to ->
-                        K2MultiplatformStructure.RefinesEdge(kotlinDependency.name, to.name)
+        compileTask.compilerOptions.freeCompilerArgs.addAll(compileTask.isK2.flatMap { isK2 ->
+            if (isK2) {
+                val k2Args = objectFactory.listProperty(String::class.java)
+
+                k2Args.add("-Xfragment-refines=${kotlinSourceSet.name}:${kotlinDependency.name}")
+
+                for (fragment in newFragments) {
+                    k2Args.add("-Xfragments=${fragment.name}")
+
+                    for (sourceDirectory in fragment.kotlin.sourceDirectories.asFileTree) {
+                        k2Args.add("-Xfragment-sources=${fragment.name}:$sourceDirectory")
                     }
                 }
+
+                k2Args.addAll(weakLinks.map {
+                    it.map {
+                        "-Xfragment-refines=${kotlinDependency.name}:${it.name}"
+                    }
+                })
+
+                k2Args
             } else {
                 emptyList
             }
         })
 
-        commonSourceSet.get(compileTask).from(kotlinDependency.kotlin)
+        compileTask.compilerOptions.freeCompilerArgs.addAll(providerFactory.provider {
+            kotlinDependency.kotlin.sourceDirectories.asFileTree.map {
+                "-Xcommon-sources=${it}"
+            }
+        })
+
         compileTask.source(kotlinDependency.kotlin)
 
         dependency.extensions.getByType<SourceSetStaticLinkageInfo>().links.all {
-            dependency.addKotlinCommonSources(kotlin, providerFactory, this, info, compileTask)
+            dependency.addKotlinCommonSources(
+                kotlin,
+                objectFactory,
+                providerFactory,
+                this,
+                info,
+                compileTask,
+                handledSourceSetNames
+            )
         }
     }
 
@@ -148,9 +152,16 @@ open class JavaVirtualSourceSetsPlugin @Inject constructor(private val modelBuil
             kotlinCompilation.compileTaskProvider.configure {
                 this as KotlinCompile
 
-                this.multiPlatformEnabled.set(true)
+                compilerOptions.freeCompilerArgs.add("-Xmulti-platform")
 
-                addKotlinCommonSources(kotlin, project.serviceOf(), dependency, info, this)
+                addKotlinCommonSources(
+                    kotlin,
+                    project.serviceOf(),
+                    project.serviceOf(),
+                    dependency,
+                    info,
+                    this,
+                )
             }
         }
     }
